44be030c5cd32d6461010015497bb8d618ad0df4
surrus <xsurrus@gmail.com>
2015-12-24 04:56:37 -0700

    beautifulsoup

--- sandbox.py
+++ sandbox.py
@@ -1,4 +1,11 @@
 import requests
+from bs4 import BeautifulSoup
 
-r = requests.get('https://api.github.com/events')
-r.text
+url = "https://api.github.com/events"
+
+request = requests.get(url)
+data = request.text
+soup = BeautifulSoup(data)
+
+for link in soup.find_all('a'):
+  print(link.get('href'))
