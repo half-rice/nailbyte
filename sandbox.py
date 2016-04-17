7521347abfc484afae80ec5aeb8ab77ce251412f
surrus <xsurrus@gmail.com>
2016-04-17 05:13:15 -0600

    init creation of gui

--- sandbox.py
+++ sandbox.py
@@ -1,11 +1,24 @@
+import sys
 import requests
+from PyQt5.QtWidgets import QApplication, QWidget
 from bs4 import BeautifulSoup
 
-url = "https://google.com"
+# url = "https://google.com"
 
-r = requests.get(url)
-data = r.text
-soup = BeautifulSoup(data, "html.parser")
+# r = requests.get(url)
+# data = r.text
+# soup = BeautifulSoup(data, "html.parser")
 
-for link in soup.find_all('a'):
-  print(link.get('href'))
+# for link in soup.find_all('a'):
+#   print(link.get('href'))
+
+if __name__ == "__main__":
+  app = QApplication(sys.argv)
+
+  w = QWidget()
+  w.resize(250,150)
+  w.move(300,300)
+  w.setWindowTitle("nailbyte")
+  w.show()
+
+  sys.exit(app.exec_())
